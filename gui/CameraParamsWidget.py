68a3f23cd7aaccd501531b83cde327cdb3f3219c
s <salehahr@users.noreply.github.com>
2021-12-03 01:07:07 +0100

    implemented validators for DEA form

--- gui/CameraParamsWidget.py
+++ gui/CameraParamsWidget.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from PyQt5.QtGui import QDoubleValidator
 from PyQt5.QtWidgets import QWidget, QGroupBox, QFormLayout, QLineEdit, QPushButton
 
 
@@ -20,9 +21,18 @@ class CameraParamsWidget(QGroupBox):
 
     def _init_dea(self, camera_params: list) -> None:
         camera_params_str = [f'{x:000.1f}' for x in camera_params]
+
         self._distance = QLineEdit(camera_params_str[0])
         self._elevation = QLineEdit(camera_params_str[1])
         self._azimuth = QLineEdit(camera_params_str[2])
+
+        validator_distance = QDoubleValidator(0, 10, 2)
+        validator_elevation = QDoubleValidator(0, 90, 2)
+        validator_azimuth = QDoubleValidator(0, 360, 2)
+
+        self._distance.setValidator(validator_distance)
+        self._elevation.setValidator(validator_elevation)
+        self._azimuth.setValidator(validator_azimuth)
 
     def _init_layout(self) -> None:
         layout = QFormLayout()
